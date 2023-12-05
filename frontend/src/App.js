f30eebf1b17c228a5b4c9bfe67b06dd538ed47fe
Varun <varunguleria1700@gmail.com>
2023-12-05 15:08:18 +0530

    Structuring + Home Page + Items Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/shop'
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
     <div>
-      <Navbar />
+     
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
+          <Route path='/mens' element={<ShopCategory category="men"/>}></Route>
+          <Route path='/womens' element={<ShopCategory category="women"/>}></Route>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}></Route>
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/login' element={<LoginSignup/>}></Route>
+          </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
