6927c786e2bc6a0d2363d9dc80a46451a2004d28
Varun <varunguleria1700@gmail.com>
2023-12-07 13:42:15 +0530

    setting up men-women-kids categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
 function App() {
   return (
     <div>
@@ -15,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop/>}></Route>
-          <Route path='/mens' element={<ShopCategory category="men"/>}></Route>
-          <Route path='/womens' element={<ShopCategory category="women"/>}></Route>
-          <Route path='/kids' element={<ShopCategory category="kid"/>}></Route>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}></Route>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}></Route>
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}></Route>
           <Route path="/product" element={<Product/>}>
             <Route path=":productId" element={<Product/>}/>
           </Route>
