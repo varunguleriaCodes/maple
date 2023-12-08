431e60a6c6a3285d4baa142ff0bda9f3e342d053
Varun <varunguleria1700@gmail.com>
2023-12-09 00:38:32 +0530

    individual-components

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import {ShopContext} from '../Context/ShopContext'
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrums";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
 const Product=()=>{
+    const {all_product}=useContext(ShopContext);
+    const {productId}=useParams();
+    const product=all_product.find((e)=>e.id === Number(productId))
     return(
         <div>
-
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
+            <DescriptionBox/>
         </div>
     )
 }
